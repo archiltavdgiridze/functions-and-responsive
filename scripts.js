93228cac9f070c04b52de8d9c743519a64308149
Archil Tavdgiridze <achitavdgiridze@gmail.com>
2024-01-23 19:42:29 +0400

    finished lecture code about js functions and responsive web-design

---
+++ scripts.js
@@ -0,0 +1,57 @@
+// function test() {
+//   console.log(1);
+//   console.log(2);
+//   console.log(3);
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//   test();
+// }
+
+// function misalmeba(saxeli, asaki) {
+//   alert(`გამარჯობა ${saxeli}, შენ ხარ ${asaki} `);
+// }
+
+// let saxeli = prompt("შეიყვანე სახელი");
+// let asaki = prompt("შეიყვანე ასაკი");
+
+// misalmeba(saxeli, asaki);
+
+// function mimateba(a, b) {
+//   let plus = a + b;
+//   return plus;
+// }
+
+// console.log(mimateba(2, 5));
+
+// let globaluri = "me var globaluri cvladi"
+
+// function testFunction() {
+//     console.log(globaluri);
+// }
+
+// testFunction()
+
+// console.log(globaluri);
+
+// function testLocal() {
+//   let lokaluri = "me var lokaluri cvladi";
+//   console.log(lokaluri);
+// }
+
+// testLocal();
+// console.log(lokaluri);
+
+// function gamravleba(a, b, c) {
+//   return a * b * c;
+// }
+
+// let pasuxi = gamravleba(4, 1, 2);
+// console.log(pasuxi);
+
+// ARROW FUNCTIONS
+// let gamravleba = (x, y) => x * y;
+
+// let pasuxi = gamravleba(3, 4);
+
+// console.log(pasuxi);
